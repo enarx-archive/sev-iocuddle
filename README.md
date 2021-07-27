5f9365d43a99005bbb023b3fb1a0c4558da45fa4
Tyler Fanelli <tfanelli@redhat.com>
2021-07-27 15:10:14 -0400

    Change README + Initial boilerplate
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sev-iocuddle
-Helpful abstractions for issuing ioctls to AMD SEV platforms
+
+`sev-iocuddle` provides a set of helpful abstractions used for issuing ioctls across the AMD
+SEV platform. It is mainly used by Enarx's `sev` and `snp` crates.
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! `sev-iocuddle` provides a set of helpful abstractions used for issuing ioctls across the AMD
+//! SEV platform. It is mainly used by Enarx's `sev` and `snp` crates.
+
+#![deny(clippy::all)]
+#![allow(unknown_lints)]
+#![allow(clippy::identity_op)]
+#![allow(clippy::unreadable_literal)]
