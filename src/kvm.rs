b83b894e42adc4abe51c3481d0d144b2f3c2f367
Tyler Fanelli <tfanelli@redhat.com>
2021-08-17 13:26:27 -0400

    kvm: add KVM related shared code
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

---
+++ src/kvm.rs
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::error::*;
+use crate::sev::Id;
+
+use iocuddle::*;
+
+use std::marker::PhantomData;
+use std::os::raw::c_ulong;
+use std::os::unix::io::AsRawFd;
+
+/// The KVM iocuddle group.
+pub const KVM: Group = Group::new(0xAE);
+pub const ENC_OP: Ioctl<WriteRead, &c_ulong> = unsafe { KVM.write_read(0xBA) };
+
+/// The Rust-flavored, FFI-friendly version of `struct sev_issue_cmd` which is
+/// used to pass arguments to the SEV ioctl implementation.
+///
+/// This struct is defined in the Linux kernel: include/uapi/linux/psp-sev.h
+#[repr(C, packed)]
+pub struct Command<'a, T: Id> {
+    code: u32,
+    data: u64,
+    error: u32,
+    sev_fd: u32,
+    _phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T: Id> Command<'a, T> {
+    /// Create an SEV-SNP command with the expectation that the host platform/kernel will write to
+    /// the caller's address space either to the data held in the `Command.subcmd` field or some
+    /// other region specified by the `Command.subcmd` field.
+    pub fn from_mut(sev: &'a mut impl AsRawFd, subcmd: &'a mut T) -> Self {
+        Command {
+            code: T::ID,
+            data: subcmd as *mut T as u64,
+            error: 0,
+            sev_fd: sev.as_raw_fd() as _,
+            _phantom: PhantomData,
+        }
+    }
+
+    /// Create an SEV-SNP command with the expectation that the host platform/kernel *WILL NOT* mutate
+    /// the caller's address space in its response. Note: this does not actually prevent the host
+    /// platform/kernel from writing to the caller's address space if it wants to. This is primarily
+    /// a semantic tool for programming against the SEV-SNP ioctl API.
+    pub fn from(sev: &'a mut impl AsRawFd, subcmd: &'a T) -> Self {
+        Command {
+            code: T::ID,
+            data: subcmd as *const T as u64,
+            error: 0,
+            sev_fd: sev.as_raw_fd() as _,
+            _phantom: PhantomData,
+        }
+    }
+}
