e44ba23b6eed51978d47ce786c88b33e11e6f5cc
Tyler Fanelli <tfanelli@redhat.com>
2021-08-17 16:02:11 -0400

    util: add impl_const_id
    Slightly modified version of impl_const_id that does not initialize the trait.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/util.rs
+++ src/util.rs
@@ -5,6 +5,23 @@ use std::io::{Read, Result, Write};
 use std::mem::{size_of, MaybeUninit};
 use std::slice::{from_raw_parts, from_raw_parts_mut};
 
+#[doc(hidden)]
+#[macro_export]
+macro_rules! impl_const_id {
+    (
+     	$visibility:vis $trait:ident => $id_ty:ty;
+        $(
+            $iocty:ty = $val:expr
+        ),* $(,)*
+    ) => {
+	$(
+            impl $trait for $iocty {
+                const ID: $id_ty = $val;
+            }
+	)*
+    };
+}
+
 pub trait TypeLoad: Read {
     fn load<T: Sized + Copy>(&mut self) -> Result<T> {
         let mut t = MaybeUninit::<T>::uninit();
